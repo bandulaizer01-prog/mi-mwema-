2e08583232e610d8899fd703e5702b4baa686ec4
bandulaizer01-prog <bandulaizer01@gmail.com>
2026-02-23 21:48:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "meddy'sBot",
   botOwner: 'meddy', // Your name
-  ownerNumber: '+255620503031', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+255755459263', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
